3666f81c1b520b6fed0244b577dc07f4e9e4039f
LG <lg@s-164-67-229-30.resnet.ucla.edu>
2015-07-28 23:37:06 -0700

    short script to make identity matrix

---
+++ makeIdentityKinship.py
@@ -0,0 +1,21 @@
+# make identity matrix for specified number of individuals
+
+import sys
+
+def writeIdentity(outfName, N):
+	with open(outfName, 'w') as outf:
+		for i in range(N):
+			lineL = ['0' for i in range(N)]
+			lineL[i] = '1'
+			outf.write('\t'.join(lineL) + '\n')
+
+	return
+
+def usage():
+	print('Usage: python makeIdentityMatrix.py [output filename] [num indivs]')
+	return
+
+
+outfName = sys.argv[1]
+N = sys.argv[2]
+writeIdentity(outfName, int(N))
