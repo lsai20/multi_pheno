4a8538c58434c551197e1a6e30212a71c9e9afee
LG <lg@s-164-67-229-33.resnet.ucla.edu>
2015-08-13 12:04:42 -0700

    .

--- compute_svd.R
+++ compute_svd.R
@@ -2,7 +2,7 @@
 # see beta_setup.R for additional fxns/reading input
 
 snps.txt.file<-"GTEx_data/Lung1k.snps.txt" 
-expr.txt.file<-"GTEx_data/Lung30.expr.txt" 
+expr.txt.file<-"GTEx_data/Lung5k.expr.txt" 
 
 X<-importX(snps.txt.file)
 Y<-importY(expr.txt.file)
@@ -10,36 +10,33 @@ Y<-importY(expr.txt.file)
 # say there are n indiv, m snp, k probes. for real data, m >> k
 n<-nrow(X); m<-ncol(X); k<-ncol(Y)
 
-# say we use b components to approximate X (exact is b = min(n))
-# and c components to approximate Y
-b<-10
-c<-0
+# say we use b components to approximate X. exact is b = min(n, m).
+# and c components to approximate Y. exact is c = min(n,k)
+b<-100
+c<-min(n,k)
 
-### instead of cross prod, to get Beta, use SVD ###
-svdX <- svd(X, nu=w, nv=w) #  X = U D V'
-Dx <- diag(svdX$d[1:w])
-dim(svdX$u); dim(Dx); dim(t(svdX$v))
+### instead of cross prod, use SVD to get Beta ###
+svdX <- svd(X, nu=b, nv=b) 
+Dx <- diag(svdX$d[1:b])
 
-X2 <- svdX$u %*% Dx %*% t(svdX$v)
+X2 <- svdX$u %*% Dx %*% t(svdX$v)  # X = U D V'
+rownames(X2) <- rownames(X); colnames(X2) <- colnames(X2)
 Beta_svd <- crossprod(X2, Y)/n
 SigmaHat_svd<-findSigmaHats(X2,Y)
-Pvals_svd<-findPvals(Beta_svd, SigmaHat, n)
+Pvals_svd<-findPvals(Beta_svd, SigmaHat_svd, n)
+
+
+# TODO sort and pick top x% of snp-pheno pairs by pval
+# (todo later - sort and pick by svd threshold, see how many cross true thresh)
+
 
 
 # TODO use svd of Y or not?
-svdY <- svd(Y, nu=7, nv=9)
-dim(svdY$u); dim(diag(svdY$d)); dim(svdY$v)
-Dy <- diag(svdY$d)
-Y2 <- svdY$u %*% Dy %*% t(svdY$v)
+svdY <- svd(Y, nu=c, nv=c)
+Dy <- diag(svdY$d[1:c])
+Y2 <- svdY$u %*% Dy %*% t(svdY$v)  # Y = U D V'
 
 u1 <- svdX$u
 v1 <- Dx %*% t(svdX$v)
 u2 <- svdY$u
 v2 <- Dy %*% t(svdY$v)
-
-dim(u1); dim(v1); dim(u2); dim(v2)
-
-Beta_true = crossprod(X,Y)
-Beta_1 = t(v1) %*% t(u1) %*% u2 %*% v2
-Beta_2 = crossprod(X2, Y2)
-rownames(Beta_2) <- rownames(Beta_true); colnames(Beta_2) <- colnames(Beta_true)
