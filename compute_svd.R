a62faf2190f4fcbe704ceba7fb146387951101e7
LG <lg@s-164-67-229-33.resnet.ucla.edu>
2015-08-14 12:43:27 -0700

    .

--- compute_svd.R
+++ compute_svd.R
@@ -2,8 +2,10 @@
 # see beta_setup.R for additional fxns/reading input
 library(reshape2) # for melt
 
+
+# TODO for full file, will have to window X and/or Y
 snps.txt.file<-"GTEx_data/Lung1k.snps.txt" 
-expr.txt.file<-"GTEx_data/Lung5k.expr.txt" 
+expr.txt.file<-"GTEx_data/Lung30.expr.txt" 
 
 X<-importX(snps.txt.file)
 Y<-importY(expr.txt.file)
@@ -21,19 +23,23 @@ svdX <- svd(X, nu=b, nv=b)
 Dx <- diag(svdX$d[1:b])
 
 X2 <- svdX$u %*% Dx %*% t(svdX$v)  # X = U D V'
-rownames(X2) <- rownames(X); colnames(X2) <- colnames(X2)
+rownames(X2) <- rownames(X); colnames(X2) <- colnames(X)
 Beta_svd <- crossprod(X2, Y)/n
 SigmaHat_svd<-findSigmaHats(X2,Y)
 Pvals_svd<-findPvals(Beta_svd, SigmaHat_svd, n)
 
-# TODO add rownames to pvals_svd
+# make dataframe of snp, pheno, beta, pval
+Beta_long<-melt(Beta_svd, id.vars=1)
+Pvals_long<-melt(Pvals_svd, id.vars=1)
+Results_svd <- cbind(Beta_long, Pvals_long[,3])
+colnames(Results_svd) <- c("snpID", "gene", "beta", "pval")
+
+# sort snp-pheno pairs by pval
+Results_svd_sorted<-results_svd[order(results_svd$pval),]
 
 # TODO sort and pick top x% of snp-pheno pairs by pval
 # (todo later - sort and pick by svd threshold, see how many cross true thresh)
 
-# make dataframe of snp, pheno, pval
-Pvals_long<-melt(Pvals_svd, id.vars=1:2)
-Pvals_long2<-melt(Pvals_svd)
 # TODO use svd of Y or not?
 svdY <- svd(Y, nu=c, nv=c)
 Dy <- diag(svdY$d[1:c])
