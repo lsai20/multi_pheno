e42a157492983d18dcb3f5851594cf4c1f03a422
LG <lisagai@cs-240-165.cs.ucla.edu>
2015-08-14 17:21:27 -0700

    top % of true pvals and corresponding svd pval

--- compute_XT_Y.R
+++ compute_XT_Y.R
@@ -1,7 +1,9 @@
 # compute Beta using matrix multiplication
 # code for input and shared functions is in input.R
 
-snps.txt.file<-"GTEx_data/Lung1k.snps.txt" 
+library(reshape2)
+
+snps.txt.file<-"GTEx_data/Lung10k.snps.txt" 
 expr.txt.file<-"GTEx_data/Lung30.expr.txt" 
 
 X<-importX(snps.txt.file)
@@ -13,23 +15,35 @@ n<-nrow(X); m<-ncol(X); k<-ncol(Y)
 Beta <- crossprod(X,Y)/nrow(X)  # m x k matrix of betas
 if (FALSE){write.table(Beta, file = "Beta.Rmatrix.tsv", sep="\t")}
 
+# find sigma, pvals, thresholds
 SigmaHat<-findSigmaHats(X,Y)
 Pvals<-findPvals(Beta, SigmaHat, n)
 Thresh_Beta<-findThresholds(alpha=10^-8, n=nrow(X), SigmaHat=SigmaHat)
 SigPairs <- findSigPairs(Beta, Thresh_Beta) # list of sig snp-pheno pairs
 
+# make long dataframe of snp, pheno, beta, pval
+Beta_long<-melt(Beta, id.vars=1)
+Pvals_long<-melt(Pvals, id.vars=1)
+Results_xty <- cbind(Beta_long, Pvals_long[,3])
+colnames(Results_xty) <- c("snpID", "gene", "beta_true", "pval_true")
+
+# sort snp-pheno pairs by pval
+Results_xty_sorted<-Results_xty[order(Results_xty$pval),]
+rownames(Results_xty_sorted)<-NULL
+
 
 ## examine first gene ##
-Beta_123 <- as.matrix(Beta[,1])
-rownames(Beta_123)<-rownames(Beta); colnames(Beta_123)<-colnames(Beta)[1] 
-
-S_123<-Beta_123[,1] * sqrt(nrow(X)) / SigmaHat[,1]
-pvals_123 <- findPvals(Beta_123[,1], SigmaHat[,1], nrow(X))
-Beta_123 <- cbind(Beta_123, pvals_123)
-
-snpNo <- c(1:nrow(Beta_123));
-Beta_123 <- cbind(snpNo, Beta_123) # add row index pre-sorting
-#Beta_123 <- Beta_123[order(Beta_123[,2]),] # sort by pval
-
-if (FALSE){write.table(Beta_123, file = "Beta_first_gene.Rmatrix.tsv", sep="\t")}
-
+if (FALSE) {
+  Beta_123 <- as.matrix(Beta[,1])
+  rownames(Beta_123)<-rownames(Beta); colnames(Beta_123)<-colnames(Beta)[1] 
+  
+  S_123<-Beta_123[,1] * sqrt(nrow(X)) / SigmaHat[,1]
+  pvals_123 <- findPvals(Beta_123[,1], SigmaHat[,1], nrow(X))
+  Beta_123 <- cbind(Beta_123, pvals_123)
+  
+  snpNo <- c(1:nrow(Beta_123));
+  Beta_123 <- cbind(snpNo, Beta_123) # add row index pre-sorting
+  #Beta_123 <- Beta_123[order(Beta_123[,2]),] # sort by pval
+  
+  if (FALSE){write.table(Beta_123, file = "Beta_first_gene.Rmatrix.tsv", sep="\t")}
+}
--- compute_svd.R
+++ compute_svd.R
@@ -4,7 +4,7 @@ library(reshape2) # for melt
 
 
 # TODO for full file, will have to window X and/or Y
-snps.txt.file<-"GTEx_data/Lung1k.snps.txt" 
+snps.txt.file<-"GTEx_data/Lung10k.snps.txt" 
 expr.txt.file<-"GTEx_data/Lung30.expr.txt" 
 
 X<-importX(snps.txt.file)
@@ -13,9 +13,15 @@ Y<-importY(expr.txt.file)
 # say there are n indiv, m snp, k probes. for real data, m >> k
 n<-nrow(X); m<-ncol(X); k<-ncol(Y)
 
+# test whether top percent_true snps are in top percent_svd snps
+percent_svd <- 0.05
+percent_true <- 0.05 # 0.01
+
+
 # say we use b components to approximate X. exact is b = min(n, m).
 # and c components to approximate Y. exact is c = min(n,k)
-b<-100
+b<-70 
+# seems like a lot of improvement from b=60 to b=70 - plot/quantitfy?
 c<-min(n,k)
 
 ### instead of cross prod, use SVD to get Beta ###
@@ -28,19 +34,37 @@ Beta_svd <- crossprod(X2, Y)/n
 SigmaHat_svd<-findSigmaHats(X2,Y)
 Pvals_svd<-findPvals(Beta_svd, SigmaHat_svd, n)
 
-# make dataframe of snp, pheno, beta, pval
+# make long dataframe of snp, pheno, beta, pval
 Beta_long<-melt(Beta_svd, id.vars=1)
 Pvals_long<-melt(Pvals_svd, id.vars=1)
 Results_svd <- cbind(Beta_long, Pvals_long[,3])
-colnames(Results_svd) <- c("snpID", "gene", "beta", "pval")
+colnames(Results_svd) <- c("snpID", "gene", "beta_svd", "pval_svd")
 
 # sort snp-pheno pairs by pval
-Results_svd_sorted<-results_svd[order(results_svd$pval),]
+Results_svd_sorted<-Results_svd[order(Results_svd$pval),]
+rownames(Results_svd_sorted)<-NULL
 
-# TODO sort and pick top x% of snp-pheno pairs by pval
-# (todo later - sort and pick by svd threshold, see how many cross true thresh)
+# attach "true" (X^T*Y) beta and pvals to df
+Results_all<-cbind(Results_svd, 
+                   Results_xty[,3:4],
+                   rank(Results_svd$pval_svd),
+                   rank(Results_xty$pval_true)
+                   )
+colnames(Results_all)[7] <- "rank_pval_svd"
+colnames(Results_all)[8] <- "rank_pval_true"
+
+# Pick top percent_true of snp-pheno pairs by true pval
+# Are they ranked highly by svd pval as well?
+topTrue <- Results_all[which(Results_all$rank_pval_true <= percent_true*m),]
+topTrue <- topTrue[order(topTrue$rank_pval_true),]
+
+# TODO get percentiles instead of rank
+
+# (TODO later - sort and pick by svd threshold, see how many cross true thresh)
+# (TODO get theoretical thresh)
 
 # TODO use svd of Y or not?
+if (FALSE){
 svdY <- svd(Y, nu=c, nv=c)
 Dy <- diag(svdY$d[1:c])
 Y2 <- svdY$u %*% Dy %*% t(svdY$v)  # Y = U D V'
@@ -49,3 +73,4 @@ u1 <- svdX$u
 v1 <- Dx %*% t(svdX$v)
 u2 <- svdY$u
 v2 <- Dy %*% t(svdY$v)
+}
