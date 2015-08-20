56df25ff1e4648697e2edf87a27143c145dbe0f2
LG <lisagai@cs-240-165.cs.ucla.edu>
2015-08-20 16:01:35 -0700

    .

--- svd_thresh_targetRR.R
+++ svd_thresh_targetRR.R
@@ -11,5 +11,15 @@ rank_X = 100        # rank of svdX (exact is 119)
 # find 99% thresh (i.e. thresh for pval_svd st. pval_svd < thresh contains 99% of true)
 
 # eventually for loop for rank_X ranging 1 to 119
+#want 99% of true pvals to have p_svd within thresh_svd
+#any 99% of true pvals ok. (i.e if 100 true vals, okay to get any 99, not top 99)
 
- 
+# TODO
+# test on small data set - 1k snp, 30 pheno
+
+topTrue <- Results_all[which(Results_all$pval_true <= thresh_true),]
+topTrue <- topTrue[order(topTrue$pval_svd),]
+
+cor(Results_all$pval_svd, Results_all$pval_true)
+cor(topTrue$pval_svd, topTrue$pval_true)
+
